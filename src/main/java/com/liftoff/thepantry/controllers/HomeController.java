6f3cbf7faab61449d69e97a9c0e6b03fb8733d51
James <jamzdarren@gmail.com>
2022-08-11 17:31:22 -0500

    updated controller, repository, and recipe model

--- src/main/java/com/liftoff/thepantry/controllers/HomeController.java
+++ src/main/java/com/liftoff/thepantry/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package com.liftoff.thepantry.controllers;
 
+import com.liftoff.thepantry.models.Recipe;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,18 +8,25 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HelloController {
+public class HomeController {
 
-//    @GetMapping("/")
-//    public String hello() {
-//        return "index";
-//    }
-//
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/add")
+    public String  displayAddRecipe(Model model) {
+        model.addAttribute("title", "Add Recipe");
+        model.addAttribute(new Recipe());
+        return "add";
+    }
+
 //    @GetMapping("greetme")
 //    public String greetme() {
 //        return "greetme";
 //    }
-//
+
 //    @PostMapping("greetme")
 //    public String greetMe(@RequestParam String name, Model model) {
 //        String greeting = "Hello, " + name + "!";
