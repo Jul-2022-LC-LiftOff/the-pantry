027a61676e5c0de43de40f3885c2b253d415ef6d
James <jamzdarren@gmail.com>
2022-08-11 18:14:43 -0500

    updated form connection to db

--- src/main/java/com/liftoff/thepantry/controllers/HomeController.java
+++ src/main/java/com/liftoff/thepantry/controllers/HomeController.java
@@ -1,19 +1,32 @@
 package com.liftoff.thepantry.controllers;
 
+import com.liftoff.thepantry.data.RecipeRepository;
 import com.liftoff.thepantry.models.Recipe;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
 @Controller
 public class HomeController {
+
+    @Autowired
+    private RecipeRepository recipeRepository;
+
 
     @GetMapping("/")
     public String index() {
         return "index";
     }
+
 
     @GetMapping("/add")
     public String  displayAddRecipe(Model model) {
@@ -22,16 +35,24 @@ public class HomeController {
         return "add";
     }
 
-//    @GetMapping("greetme")
-//    public String greetme() {
-//        return "greetme";
-//    }
+    @PostMapping("add")
+    public String processAddRecipe(@ModelAttribute @Valid Recipe newRecipe,
+                                    Errors errors, Model model, @RequestParam String ingredients, @RequestParam String instructions) {
 
-//    @PostMapping("greetme")
-//    public String greetMe(@RequestParam String name, Model model) {
-//        String greeting = "Hello, " + name + "!";
-//        model.addAttribute("greeting", greeting);
-//        return "greetme";
-//    }
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Recipe");
+
+            return "add";
+        }
+
+        newRecipe.setIngredients(ingredients);
+
+        newRecipe.setInstructions(instructions);
+
+        recipeRepository.save(newRecipe);
+
+        return "redirect:";
+    }
+
 
 }
