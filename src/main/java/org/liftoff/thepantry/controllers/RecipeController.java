cdd6d35a2716a3e57b92d2fd619e59a4643e8307
James <jamzdarren@gmail.com>
2022-09-23 00:34:39 -0500

    save

--- src/main/java/org/liftoff/thepantry/controllers/RecipeController.java
+++ src/main/java/org/liftoff/thepantry/controllers/RecipeController.java
@@ -4,7 +4,10 @@ import org.liftoff.thepantry.data.RecipeIngredientRepository;
 import org.liftoff.thepantry.data.RecipeRepository;
 import org.liftoff.thepantry.models.Recipe;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
 import java.util.Optional;
 
 @Controller
@@ -43,7 +45,7 @@ public class RecipeController {
     @GetMapping("recipes")
     public String displayRecipes(HttpServletRequest request, Model model) {
         int page = 0;
-        int size = 4;
+        int size = 12;
 
         if (request.getParameter("page") != null && !request.getParameter("page").isEmpty()) {
             page = Integer.parseInt(request.getParameter("page")) - 1;
@@ -59,44 +61,38 @@ public class RecipeController {
         return "recipes";
     }
 
-    @PostMapping("recipes/results")
-    public String displayRecipesBySearch(Model model, @RequestParam String searchTerm, RedirectAttributes ra) {
-        ArrayList<Recipe> recipes;
+    @PostMapping("recipes")
+    public String displayRecipesBySearch(HttpServletRequest request, Model model, @RequestParam String searchTerm, RedirectAttributes ra, Sort sort) {
+        Page<Recipe> recipes;
+
+        int page = 0;
+        int size = 12;
+
+        if (request.getParameter("page") != null && !request.getParameter("page").isEmpty()) {
+            page = Integer.parseInt(request.getParameter("page")) - 1;
+        }
+        if (request.getParameter("size") != null && !request.getParameter("size").isEmpty()) {
+            size = Integer.parseInt(request.getParameter("size"));
+        }
+
         if (searchTerm.toLowerCase().isEmpty() || searchTerm.isBlank() || searchTerm.equals("")) {
             ra.addFlashAttribute("class", "alert alert-danger");
             ra.addFlashAttribute("message", "Please enter a search term.");
             return "redirect:/recipes";
         } else {
-            recipes = findByValue(searchTerm, recipeRepository.findAll());
+            Pageable pageable = PageRequest.of(page, size);
+            recipes = recipeRepository.findByInstructionsContaining(searchTerm, pageable);
             if (recipes.isEmpty()) {
                 ra.addFlashAttribute("class", "alert alert-danger");
-                ra.addFlashAttribute("message", "No results for '" + searchTerm + "'");
+                ra.addFlashAttribute("message", "No results matching '" + searchTerm + "'");
                 return "redirect:/recipes";
             }
             model.addAttribute("title", "Recipes matching '" + searchTerm +"'");
         }
+
         model.addAttribute("banner", "recipes");
         model.addAttribute("recipes", recipes);
         return "recipes";
-    }
-
-    // find recipes by search term
-
-    public static ArrayList<Recipe> findByValue(String aValue, Iterable<Recipe> allRecipes) {
-        String value = aValue.toLowerCase();
-        ArrayList<Recipe> results = new ArrayList<>();
-
-        for (Recipe recipe : allRecipes) {
-            if (recipe.getName().toLowerCase().contains(value)) {
-                results.add(recipe);
-            } else if (recipe.getDescription().toString().toLowerCase().contains(value)) {
-                results.add(recipe);
-            } else if (recipe.getInstructions().toString().toLowerCase().contains(value)) {
-                results.add(recipe);
-            }
-        }
-
-        return results;
     }
 
 }
